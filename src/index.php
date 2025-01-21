6bfc01e638cea847c702cb725e407785e4741f2b
whrksp126 <whrksp126@gmail.com>
2025-01-22 02:09:18 +0900

    save

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ try {
     $dbname = getenv('MYSQL_DATABASE');
     $user = getenv('MYSQL_USER');
     $pass = getenv('MYSQL_PASSWORD');
-    
+    echo "단(근)디... <br>";
     echo "연결 시도 중... <br>";
     echo "Host: " . ($host ?: 'not set') . "<br>";
     echo "Database: " . ($dbname ?: 'not set') . "<br>";
@@ -13,7 +13,7 @@ try {
     $dsn = "mysql:host=$host;dbname=$dbname";
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "MySQL 연결 성공!";
+    echo "MySQL 연결 성공! ";
 } catch(PDOException $e) {
     echo "연결 실패: " . $e->getMessage();
 }
